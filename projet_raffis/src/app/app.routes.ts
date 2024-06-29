af0a714e9c1d960778a9f4e5b066ecca2d457534
Cerveau70 <allotokoo@gmail.com>
2024-06-29 00:13:18 +0000

    premiere mise à jour du projet interface de connection et inscription

--- projet_raffis/src/app/app.routes.ts
+++ projet_raffis/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+   import { Routes, RouterModule } from '@angular/router';
+   import { LoginComponent } from './page/login/login.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+   export const routes: Routes = [
+     { path: '', component: LoginComponent },
+     { path: '', component: DashboardComponent},
+     // Autres routes de votre application
+   ];
+
+   @NgModule({
+     imports: [RouterModule.forRoot(routes)],
+     exports: [RouterModule]
+   })
+   export class AppRoutingModule { }
